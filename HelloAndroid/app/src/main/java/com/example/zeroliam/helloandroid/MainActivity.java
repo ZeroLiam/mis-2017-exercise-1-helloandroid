787f50930f719cbc952eb7edc15190b7d099c6e9
ZeroLiam <liselotr@gmail.com>
2017-04-17 21:27:38 +0200

    HelloAndroid - Working on the URL and HTTP Connection

--- HelloAndroid/app/src/main/java/com/example/zeroliam/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/com/example/zeroliam/helloandroid/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.zeroliam.helloandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.io.*;
 import java.net.*;
@@ -11,7 +13,7 @@ import java.net.*;
 public class MainActivity extends AppCompatActivity {
     //Layout elements here
     Button btn;
-    TextView geturl;
+    EditText geturl;
     TextView displayurl;
 
     @Override
@@ -21,24 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
         //Get the elements from the XML
         btn = (Button) findViewById(R.id.reqBtn);
-        geturl = (TextView) findViewById(R.id.reqURL);
+        geturl = (EditText) findViewById(R.id.reqURL);
         displayurl = (TextView) findViewById(R.id.displayHTML);
 
         //Setting the click listener
         btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 //Setting the URL & HTTPS requests
-
+                String urltxt = geturl.getText().toString();
+                Log.e("HelloAndroid", urltxt);
                 try{
-                    URL myurl = new URL("hsjafg");
-                    URLConnection connection = myurl.openConnection();
+                    URL theurl = new URL (urltxt);
+                    URLConnection urlcon = theurl.openConnection();
+                    Object responseCode = 0;
+                    urlcon.connect();
+                    Log.e("HelloAndroid", "ON THE TRY SIDE");
+                    responseCode = urlcon.getRequestProperties();
 
-                }catch(IOException e){
-                    e.printStackTrace();
-                    System.out.println("EXCEPTIONNN!!!");
+                    Log.e("HelloAndroid", responseCode.toString());
+//                    System.out.println(responseCode.toString());
 
+                }catch (IOException ioe){
+                    Log.e("HelloAndroid", "CALLING STACK TRACE");
+                    ioe.printStackTrace();
                 }
-
             }
         });
 
