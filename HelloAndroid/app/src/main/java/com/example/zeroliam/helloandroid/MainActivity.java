f8ee1f73da43825f3a0ed979d2b77a41aa6ab16e
ZeroLiam <liselotr@gmail.com>
2017-04-18 08:23:05 +0200

    HelloAndroid - Separated connection and input stream in methods; discovered a need for a new thread; starting to debug and handle final errors

--- HelloAndroid/app/src/main/java/com/example/zeroliam/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/com/example/zeroliam/helloandroid/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     //Layout elements here
@@ -29,27 +30,100 @@ public class MainActivity extends AppCompatActivity {
         //Setting the click listener
         btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                //Setting the URL & HTTPS requests
-                String urltxt = geturl.getText().toString();
-                Log.e("HelloAndroid", urltxt);
-                try{
-                    URL theurl = new URL (urltxt);
-                    URLConnection urlcon = theurl.openConnection();
-                    Object responseCode = 0;
-                    urlcon.connect();
-                    Log.e("HelloAndroid", "ON THE TRY SIDE");
-                    responseCode = urlcon.getRequestProperties();
-
-                    Log.e("HelloAndroid", responseCode.toString());
-//                    System.out.println(responseCode.toString());
-
-                }catch (IOException ioe){
-                    Log.e("HelloAndroid", "CALLING STACK TRACE");
-                    ioe.printStackTrace();
-                }
+                //Call the site and display it
+                displayURL(geturl.getText().toString(), displayurl);
             }
         });
+    }
 
+
+    /**
+     * Method name: connection
+     * Modifier:    private
+     * Purpose:     To connect to a URL, make a connection, grab the object and pass it
+     *              to a InputStream that will be returned, ready to be used later.
+     * Parameters:  String
+     * Returns:     InputStream
+     */
+    private InputStream connection (String urls){
+        //Initialize the stream that will return the html object
+        InputStream theStream = null;
+
+        //Setting the URL & HTTPS requests
+        try{
+            Log.e("HelloAndroid", "ON THE TRY SIDE");
+            //Get the url
+            URL theurl = new URL (urls);
+            Log.e("THE URL IS: ", theurl.toString());
+            //open an url connection
+            URLConnection urlcon = theurl.openConnection();
+            //pass the url connection to a httpurl connection
+            HttpURLConnection httpcon = (HttpURLConnection) urlcon;
+
+            //connect
+            httpcon.connect();
+            String responsemsg = httpcon.getResponseMessage();
+            Log.e("Response Message: ", responsemsg);
+            //get our data
+//            theStream = httpcon.getInputStream();
+
+        }catch (MalformedURLException mal){
+            Log.e("HelloAndroid", "MALFORMED URL EXCEPTION");
+            mal.printStackTrace();
+        }catch (IOException ioe){
+            Log.e("HelloAndroid", "IO EXCEPTION");
+            Log.e("IOException stack: ", ioe.toString());
+        }
+
+        return theStream;
+    }
+
+
+    /**
+     * Method name: displayURL
+     * Modifier:    public
+     * Purpose:     To to get any URL as an InputStream and display the object returned as
+     *              an HTML in a TextView.
+     * Parameters:  String, TextView source target
+     * Returns:     void
+     */
+    public void displayURL (String theurl, TextView displaypage){
+        final String finalurl = theurl;
+
+
+        //To avoid the fatal error network on main thread, we need to make a new thread
+        //(source: Defining and starting a thread - https://docs.oracle.com/javase/tutorial/essential/concurrency/runthread.html)
+        //which basically will open a new thread instead of making a new process or interfering with
+        //the main thread, and will let the connection to run in the background while we modify our
+        //visual or in early cases, do a Log.e and see what's happening.
+        //This only happens in Honeycomb or higher (source: https://developer.android.com/reference/android/os/NetworkOnMainThreadException.html#NetworkOnMainThreadException() );
+
+        (new Thread(){
+            public void run(){
+                //Initialize the local InputStream and the Scanner to read the lines from the stream
+                InputStream geturl = null;
+                Scanner linereader;
+                //Have a String to parse the HTML data
+                String receiveHTML = "";
+
+                //Call the input stream from the URL
+                //(it needs to be called final from within this inner class (IDE showed error)
+                geturl = connection(finalurl);
+                //Display the url site
+                try{
+                    //Read the lines from the input
+                    linereader = new Scanner(geturl);
+                    while(linereader.hasNext()){
+                        receiveHTML += linereader.nextLine();
+                    }
+
+                    Log.e("READING: ", receiveHTML);
+                }catch (Exception evt){
+                    evt.printStackTrace();
+                }
+            }
+        }).start();
 
     }
+
 }
